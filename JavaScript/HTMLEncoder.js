5a554db4b0aa8bea6354af806a9f30363a39af5b
Akila R Hettiarachchi <akilarandil@gmail.com>
2017-09-22 00:25:11 +0530

    Added request iteration functionality and positive integer check on iterations

--- JavaScript/HTMLEncoder.js
+++ JavaScript/HTMLEncoder.js
@@ -1,19 +1,16 @@
 function getHTML() {
     clearData();
-    let startTime = new Date();
-    getResponse("HTML", startTime)
+    positiveIntegerCheck("HTML");
 }
 
 function getJSON() {
     clearData();
-    let startTime = new Date();
-    getResponse("JSON", startTime);
+    positiveIntegerCheck("JSON");
 }
 
 function getHTMLEncoder() {
     clearData();
-    let startTime = new Date();
-    getResponse("HTMLEncoder", startTime)
+    positiveIntegerCheck("HTMLEncoder");
 }
 
 function clearData() {
@@ -21,49 +18,69 @@ function clearData() {
     $("#Time").empty();
 }
 
-function getResponse(type, startTime) {
+function positiveIntegerCheck(type) {
+    let iterations = parseInt(document.getElementById("Iterations").value);
+    if (iterations <= 0 || document.getElementById("Iterations").value === "") {
+        alert("Please Enter a Positive Integer");
+    }
+    else {
+        performIterations(type)
+    }
+}
+
+function performIterations(type) {
+    let iterations = parseInt(document.getElementById("Iterations").value);
+    let totalTimeOfAllIterations = 0;
+
+    for (i = 0; i < iterations; i++) {
+        let startTime = new Date();
+        getResponse(type);
+        totalTimeOfAllIterations = totalTimeOfAllIterations + (new Date().getTime() - startTime.getTime());
+    }
+
+    $("#Time").html("Average Time per request= " + (totalTimeOfAllIterations / iterations) * 1000 + " micro seconds");
+}
+
+function getResponse(type) {
 
     $.ajax({
         url: "../PHP/HTMLEncoder.php?type=" + type,
         success: function (result) {
-            let process = new ProcessResult(type, result, startTime);
+            let process = new ProcessResult(type, result);
             process.processData();
-
         }
     });
 }
 
 class ProcessResult {
 
-    constructor(type, result, startTime) {
+    constructor(type, result) {
         this.type = type;
         this.result = result;
-        this.startTime = startTime;
-    }
-    appendData(data) {
-        $("#Result").html(data);
-        let totalTime = new Date().getTime() - this.startTime.getTime();
-        $("#Time").html("Total Time = " + totalTime + " ms");
     }
 
-    processData(){
-        if(this.type === "HTML"){
-            this.appendData(this.result)
+    appendData(data) {
+        $("#Result").html(data);
+    }
+
+    processData() {
+        if (this.type === "HTML") {
+            this.appendData(this.result);
         }
-        else if(this.type === "JSON"){
+        else if (this.type === "JSON") {
 
             let jsonData = JSON.parse(this.result);
             let htmlData =
                 "<table>" +
-                    "<tr>" +
-                        "<th>Name</th>" +
-                        "<th>Age</th>" +
-                        "<th>City</th>" +
-                    "</tr><tr>" +
-                     "<td>" + jsonData["name"] + "</td>" +
-                    "<td>" + jsonData["age"] + "</td>" +
-                    "<td>" + jsonData["city"] + "</td>" +
-                    "</tr>" +
+                "<tr>" +
+                "<th>Name</th>" +
+                "<th>Age</th>" +
+                "<th>City</th>" +
+                "</tr><tr>" +
+                "<td>" + jsonData["name"] + "</td>" +
+                "<td>" + jsonData["age"] + "</td>" +
+                "<td>" + jsonData["city"] + "</td>" +
+                "</tr>" +
                 "</table>";
             this.appendData(htmlData)
         }
